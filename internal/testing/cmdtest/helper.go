9368f8b5ee619986c9191c4d6c62f9bfe192ae07
Timo Furrer <tfurrer@gitlab.com>
2026-01-15 13:53:37 +0100

    refactor(testing): move iostreams test helper to dedicated file

--- internal/testing/cmdtest/helper.go
+++ internal/testing/cmdtest/helper.go
@@ -104,35 +104,6 @@ func InitTest(m *testing.M, suffix string) {
 	os.Exit(code)
 }
 
-// WithTestIOStreamsAsTTY sets stdin, stdout and stderr as TTY
-// By default they are not treated as TTYs. This will overwrite the behavior
-// for the three of them. If you only want to set a specific one,
-// use iostreams.WithStdin, iostreams.WithStdout or iostreams.WithStderr.
-func WithTestIOStreamsAsTTY(asTTY bool) iostreams.IOStreamsOption {
-	return func(i *iostreams.IOStreams) {
-		i.IsInTTY = asTTY
-		i.IsaTTY = asTTY
-		i.IsErrTTY = asTTY
-	}
-}
-
-func TestIOStreams(options ...iostreams.IOStreamsOption) (*iostreams.IOStreams, *bytes.Buffer, *bytes.Buffer, *bytes.Buffer) {
-	in := &bytes.Buffer{}
-	out := &bytes.Buffer{}
-	errOut := &bytes.Buffer{}
-
-	opts := []iostreams.IOStreamsOption{
-		iostreams.WithStdin(io.NopCloser(in), false),
-		iostreams.WithStdout(out, false),
-		iostreams.WithStderr(errOut, false),
-	}
-	opts = append(opts, options...)
-
-	ios := iostreams.New(opts...)
-
-	return ios, in, out, errOut
-}
-
 type Factory struct {
 	ApiClientStub    func(repoHost string) (*api.Client, error)
 	GitLabClientStub func() (*gitlab.Client, error)
